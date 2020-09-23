ccb140c9a5a19e33d1d157fde5e3ae9015fa08b1
beto1 <beto1@DESKTOP-46J40AI>
2020-09-23 13:17:14 -0500

    Upload

---
+++ SumaN/src/Sumatoria/Sumas.java
@@ -0,0 +1,25 @@
+package Sumatoria;
+
+import java.util.Scanner;
+
+public class Sumas {
+
+	public static void main(String[] args) {
+
+		@SuppressWarnings("resource")
+		Scanner reader = new Scanner(System.in);
+		int numero1 = 0;
+		int numero2 = 0;
+		int resultado = 0;
+			
+		System.out.println("Introduce el primer número:");			
+		numero1 = reader.nextInt();
+					
+		System.out.println("Introduce el segundo número:");
+		numero2 = reader.nextInt();
+		resultado = numero1+numero2;
+		System.err.println("La suma es " + numero1 + " + " + numero2 + " = " + resultado);
+
+	}
+
+}
